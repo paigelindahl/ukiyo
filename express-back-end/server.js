81e931c31c17c70b4859782eaf3c34d6c854b06c
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-11-23 22:50:55 +0000

    BE is now talking to DB, and we can post and get from DB to BE v/v using postman

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,7 +2,7 @@ const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
 const db = require('./db');
-// const dbHelpers = require('./helpers/dbHelpers')(db);
+const dbHelpers = require('./helpers/dbHelpers')(db);
 
 
 const PORT = 8080;
@@ -13,6 +13,7 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
 
+
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
@@ -22,3 +23,28 @@ App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
 });
+
+App.post("/dashboard", async (req, res) => {
+
+  const {task, task_title, created_at, completed, user_id} = req.body;
+  console.log(typeof user_id)
+
+
+
+  try {
+    const newTodo = await db.query("INSERT INTO tasks(task, task_title, created_at, completed, user_id) VALUES($1, $2, $3, $4, $5) RETURNING *", [task, task_title, created_at, completed, user_id])
+    console.log('this is ressend', newTodo.rows)
+  } catch (error) {
+    console.error(err.message)
+  }
+})
+
+App.get('/dashboard', async (req, res) => {
+  try {
+    const allTodos = await db.query("SELECT * FROM tasks");
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+
+})
