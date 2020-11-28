c1111ad85831bc1b3c0010a4ad2fdcc3563819f8
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-11-28 19:15:10 +0000

    main page css complete

--- express-back-end/db/seeds/03_quotes.sql
+++ express-back-end/db/seeds/03_quotes.sql
@@ -1,7 +1,7 @@
 INSERT INTO quotes(content, author, user_id, is_favourited)
 VALUES
-('This is a deep quote', 'Kourtney', 1, true),
-('This is a deeper quote', 'Huget', 1, true),
-('This is the deepest quote', 'Paige', 1, true),
-('This is a shallow quote', 'Lindahl', 1, false),
-('This is a shallower quote', 'Saad', 1, false);
+('Do, or do not. There is no try.', 'Yoda', 1, true),
+('Courage is going from failure to failure without losing enthusiasm.', 'Winston Churchill', 1, true),
+('', 'Lindahl', 1, true),
+('One fails forward toward success.', 'Charles Kettering', 1, true);
+('Learn from yesterday, live for today, hope for tomorrow.', 'Albert Einstein', 1, true),
