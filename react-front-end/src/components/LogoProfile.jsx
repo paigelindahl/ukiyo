c86a4a63edf1543173d182fe04ab819a87ca49ba
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-11-28 18:16:26 +0000

    avatar completed. puppy in place

--- react-front-end/src/components/LogoProfile.jsx
+++ react-front-end/src/components/LogoProfile.jsx
@@ -32,7 +32,7 @@ export function LogoProfile(props) {
         </Link>
         <div className="profile-info">
         <Avatar alt="Remy Sharp" 
-        src="../public/images/avatar/puppy.jpeg"
+        src="https://images.unsplash.com/photo-1593134257782-e89567b7718a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=675&q=80"
         />
           <h5 className="user-name">Paige</h5>
         </div>
