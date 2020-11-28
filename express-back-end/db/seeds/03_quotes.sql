0cd11f830cb281419bde91bf961bdb0e94e1e127
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-11-28 19:28:35 +0000

    css complete for journal 1 and 2

--- express-back-end/db/seeds/03_quotes.sql
+++ express-back-end/db/seeds/03_quotes.sql
@@ -2,6 +2,6 @@ INSERT INTO quotes(content, author, user_id, is_favourited)
 VALUES
 ('Do, or do not. There is no try.', 'Yoda', 1, true),
 ('Courage is going from failure to failure without losing enthusiasm.', 'Winston Churchill', 1, true),
-('', 'Lindahl', 1, true),
-('One fails forward toward success.', 'Charles Kettering', 1, true);
-('Learn from yesterday, live for today, hope for tomorrow.', 'Albert Einstein', 1, true),
+('Do not cry because its over. Smile because it happened.', 'Dr. Seuss', 1, true),
+('One fails forward toward success.', 'Charles Kettering', 1, true),
+('Learn from yesterday, live for today, hope for tomorrow.', 'Albert Einstein', 1, true);
