25ae800d34370c4183b07253b9dcfa86251422f9
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-12-01 02:10:42 +0000

    mt change

--- react-front-end/src/components/CustomSlider.jsx
+++ react-front-end/src/components/CustomSlider.jsx
@@ -14,7 +14,7 @@ import "./styles/CustomSlider.scss";
 const useStyles = makeStyles((theme) => ({
   root: {
     width: 200 + theme.spacing(3) * 2,
-    marginTop: "100px",
+    marginTop: "150px",
   },
 }));
 
